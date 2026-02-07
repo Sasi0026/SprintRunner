f8bd67149ab0abb9040ba559656cac69b4ef934d
Sasi0026 <shashikiranroy13@gmail.com>
2026-02-07 11:50:16 +0530

    Add Task class with to_dict and from_dict methods

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
+from src.task import Task
+
 def main():
     print('Sprint Runner Started')
+
+    name = input('Enter the Task Name : ')
+    total_duration_minutes = int(input('Enter Total Time (minutes) : '))
+    work_block_minutes = int(input('Enter work block Time (minutes) : '))
+    break_block_seconds = int(input('Enter break Time (minutes) : '))
+
+    task = Task(name,total_duration_minutes,work_block_minutes,break_block_seconds)
+
+    if task.validate():
+        print(f"\n Task Created Successfully!")
+        print(task)
+    else:
+        print("Invalid task settings!")
+
+    # Test loading from dict
+    task_dict = task.to_dict()
+    print(f"\nTask as dictionary: {task_dict}")
+
+    loaded_task = Task.from_dict(task_dict)
+    print(f"\nLoaded task: {loaded_task}")
+    #print(task.summary())
+    #print(task.validate())
+    #print(task.__str__())
 
 if __name__ == "__main__":
     main()
---
+++ src/task.py
@@ -0,0 +1,48 @@
+class Task:
+    """Represents a timer task configuration"""
+
+    def __init__(self, name, total_duration_minutes, work_block_minutes, break_block_seconds):
+        self.name = name
+        self.total_duration_minutes = total_duration_minutes
+        self.work_block_minutes = work_block_minutes
+        self.break_block_seconds = break_block_seconds
+
+    def validate(self):
+        if self.total_duration_minutes <= 0:
+            return False
+        if self.work_block_minutes <= 0:
+            return False
+        if self.break_block_seconds < 0:
+            return False
+        return True
+    
+    def to_dict(self):
+        """ generate docstring (We only need the dictionary temporarily (to save to file) )"""
+        return {
+            'name':self.name, 
+            'total_duration_minutes':self.total_duration_minutes, 
+            'work_block_minutes':self.work_block_minutes, 
+            'break_block_seconds':self.break_block_seconds
+            }
+    
+    @classmethod
+    def from_dict(cls,task_dict):
+        """Create a Task object from dictionary"""
+        return cls(
+            name=task_dict['name'],
+            total_duration_minutes=task_dict['total_duration_minutes'],
+            work_block_minutes=task_dict['work_block_minutes'],
+            break_block_seconds=task_dict['break_block_seconds']
+        )
+
+
+    def summary(self):
+        return(
+            f"\nTask: {self.name}\n"
+            f"Total Time : {self.total_duration_minutes}\n"
+            f"Sprint : {self.work_block_minutes}\n"
+            f"Break Time: {self.break_block_seconds}\n"
+        )
+            
+    def __str__(self):
+        return f"Task: {self.name} ({self.total_duration_minutes} min)"
